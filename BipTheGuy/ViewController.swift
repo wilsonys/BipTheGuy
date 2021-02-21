b4b012b3c0bde0624da39982ca4d1b9fdab53d48
Quinn Wilson <qtwilson10@gmail.com>
2021-02-21 12:54:37 -0500

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Quinn Wilson on 2/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
